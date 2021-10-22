4d5b8c9e1d58b6c51bb7423c4e041757e3f1707b
JmA515 <janderson515@yahoo.com>
2021-10-22 08:58:13 -0700

    Paired programming with Steven. Completed problem 8.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -21,8 +21,8 @@ namespace DatabaseFirstLINQ
             //ProblemFour();
             //ProblemFive();
             //ProblemSix();
-            ProblemSeven();
-            //ProblemEight();
+            //ProblemSeven();
+            ProblemEight();
             //ProblemNine();
             //ProblemTen();
             //ProblemEleven();
@@ -133,7 +133,16 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "afton@gmail.com".
             // Then print the product's name, price, and quantity to the console.
+            var customerUsers = _context.ShoppingCarts
+                                        .Include(sc => sc.User)
+                                        .Where(sc => sc.User.Email == "afton@gmail.com")
+                                        .Include(sc => sc.Product);
 
+
+            foreach (ShoppingCart product in customerUsers)
+            {
+                Console.WriteLine($"Name: {product.Product.Name} Price: {product.Product.Price} Quantity: {product.Quantity}");
+            }
         }
 
         private void ProblemNine()
